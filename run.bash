74d8be7f520b98e53139a2d121857b04de986f98
Roi Martin <jroi.martin@gmail.com>
2023-10-04 09:37:42 +0200

    lava-action: implement basic Lava action

---
+++ run.bash
@@ -0,0 +1,21 @@
+# Copyright 2023 Adevinta
+
+if [[ -z $GITHUB_OUTPUT ]]; then
+	echo 'error: missing env var GITHUB_OUTPUT' >&2
+	exit 2
+fi
+
+if [[ -z $LAVA_CONFIG ]]; then
+	echo 'error: missing env var LAVA_CONFIG' >&2
+	exit 2
+fi
+
+output=$(mktemp)
+
+lava run -c "${LAVA_CONFIG}" > "${output}"
+status=$?
+
+echo "status=${status}" >> "${GITHUB_OUTPUT}"
+echo "report=${output}" >> "${GITHUB_OUTPUT}"
+
+exit $status
